67976133e7d20b8b0d5e87b2148bdf5517a5c742
fazeel <muhamed.faseel@happiestminds.com>
2022-09-30 16:41:36 +0530

    Extra assignments

---
+++ corporateRulesInterface.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace corporateRulesInterfaceAss
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TCS tcs1 = new TCS(1223,"ramu", "sales","manager",25000);
+            Accenture acc1 = new Accenture(1223, "ramu", "sales", "manager", 25000);
+
+            Console.WriteLine($"employee pf: {tcs1.employeePF(tcs1.basicSalary)}");
+            Console.WriteLine($"gratuity amount: {tcs1.gratuityAmount(6,tcs1.basicSalary)}");
+            Console.WriteLine($"employee pf: {acc1.employeePF(acc1.basicSalary)}");
+            Console.WriteLine($"gratuity amount: {acc1.gratuityAmount(6, acc1.basicSalary)}");
+        }
+    }
+    interface CorporateRules
+    {
+         double employeePF(double basicSalary);
+         double gratuityAmount(int serviceCompleted, double basicSalary);
+    }
+    class TCS : CorporateRules
+    {
+        int empid { get; set; }
+        string empname { get; set; }
+        string dept { get; set; }
+        string desgn { get; set; }
+        public double basicSalary { get; set; }
+        public TCS(int empid, string empname, string dept, string desgn, double basicSalary)
+        {
+            this.empid = empid;
+            this.empname = empname;
+            this.dept = dept;
+            this.desgn = desgn;
+            this.basicSalary = basicSalary;
+        }
+
+        public double employeePF(double basicSalary) {
+            double pf1 = 0.12 * basicSalary;
+            double pf2= 0.0833*basicSalary;
+            double pft=pf1+ pf2 + 0.0367*basicSalary;
+            return pft;
+
+        }
+        public double gratuityAmount(int serviceCompleted, double basicSalary) {
+            double gratuity;
+            if (serviceCompleted >5 && serviceCompleted<=10)
+            {
+                gratuity = basicSalary;
+            }
+            else if (serviceCompleted >10 && serviceCompleted <= 20)
+            {
+                gratuity=basicSalary*2;
+            }
+            else if (serviceCompleted > 20)
+            {
+                gratuity = 3 * basicSalary;
+            }
+            else
+            {
+                gratuity = 0;
+            }
+            return gratuity;
+        }
+    }
+    class Accenture : CorporateRules
+    {
+        int empid { get; set; }
+        string empname { get; set; }
+        string dept { get; set; }
+        string desgn { get; set; }
+        public double basicSalary { get; set; }
+        public Accenture ( int empid, string empname, string dept, string desgn, double basicSalary)
+        {
+            this.empid = empid;
+            this.empname = empname;
+            this.dept = dept;
+            this.desgn = desgn;
+            this.basicSalary = basicSalary;
+        }
+
+        public double employeePF(double basicSalary)
+        {
+            double pf1 = 0.12 * basicSalary;
+            double pf2 = 0.12 * basicSalary;
+            double pft = pf1 + pf2;
+            return pft;
+
+        }
+        public double gratuityAmount(int serviceCompleted, double basicSalary)
+        {
+            double gratuity;
+            if (serviceCompleted > 5 && serviceCompleted <= 10)
+            {
+                gratuity = basicSalary;
+            }
+            else if (serviceCompleted > 10 && serviceCompleted <= 20)
+            {
+                gratuity = basicSalary * 2;
+            }
+            else if (serviceCompleted > 20)
+            {
+                gratuity = 3 * basicSalary;
+            }
+            else
+            {
+                gratuity = 0;
+            }
+            return gratuity;
+        }
+    }
+
+
+
+}
---
+++ mobileNumberValidationException.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mobileNumberValidation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContactDetail cd = new ContactDetail();
+            ContactDetailBO cdbo = new ContactDetailBO();
+            Console.WriteLine("Enter mobile number");
+            cd.mobile = long.Parse(Console.ReadLine());
+            Console.WriteLine("Enter alternate mobile number");
+            cd.alternateMobile = long.Parse(Console.ReadLine());
+            try
+            {
+                cdbo.validate(cd);
+            }
+            catch(DuplicateNumberException dup)
+            {
+                Console.WriteLine(dup.Message);
+            }
+        }
+    }
+
+    class ContactDetail
+    {
+        public long mobile { get; set; }
+        public long alternateMobile { get; set; }
+        long landLine { get; set; } 
+        string email { get; set; }
+        string address { get; set; }
+        //public ContactDetail(long mobile, long alternateMobile, long landLine, string email, string address)
+        //{
+        //    this.mobile = mobile;
+        //    this.alternateMobile = alternateMobile;
+        //    this.landLine = landLine;
+        //    this.email = email;
+        //    this.address = address;
+        //}
+
+    }
+    class ContactDetailBO
+    {
+        public void validate(ContactDetail cd)
+        {
+            if (cd.mobile == cd.alternateMobile)
+            {
+                DuplicateNumberException dup = new DuplicateNumberException("Exception: Same Mobile number and Alternate Mobile number.");
+                throw dup;
+            }
+            else
+            {
+                Console.WriteLine($"mobile number: {cd.mobile}");
+                Console.WriteLine($"mobile number: {cd.alternateMobile}");
+
+            }
+        }
+    }
+
+    class DuplicateNumberException:Exception
+    {
+        public DuplicateNumberException(string message) : base(message)
+        {
+
+        }
+    }
+}
