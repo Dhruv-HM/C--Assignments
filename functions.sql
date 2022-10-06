4f530b55c3bec8b837726ea4adb86f1120b1c5f7
Dhruv Kanthaliya <dhruv.kanthaliya@happiestminds.com>
2022-10-06 11:54:47 +0530

    again

---
+++ functions.sql
@@ -0,0 +1,35 @@
+﻿--Write an SQL statement to create a scalar function with name fnFactorial which should take an integer parameter and returns the factorial of the given number.
+create or alter function fnFactorial (@num int) returns int
+as begin 
+	declare @fact int, @i int
+	set @i=1
+	set @fact=1
+	while @i<=@num
+		begin 
+			set @fact=@fact*(@i)
+			set @i=@i+1
+		end
+	return @fact
+end
+
+--select *,Shopify.fnFactorial(5) as something from Employees
+
+--Write an SQL statement to create a function with name fnGetCustomersByAge which should take Age as parameter and returns the records from Customer table where age is greater than or equal to the Age parameter.
+create or alter function gnGetCustomerByAge(@Age int) returns Table
+as 
+return 
+	select * from Customers where Age>@Age
+
+--Write an SQL statement to create a function with name fnGetOldestCustomer which should return the record of the oldest customer by age.
+create or alter function fnOldestCustomer returns TABLE
+as 
+select * from Customers where Age=max(Age)
+--Write an SQL statement to alter a function fnGetCustomersByAge which should take Age as parameter and returns only the CustomerName.
+create or alter function fnGetCustomerByAge(@age int) 
+as 
+return 
+	select CustomerName from Customers where Age>@age
+
+--Write an SQL statement to delete a function fnFactorial.
+Drop Function [fnFactorial]
+
---
+++ subqueries.sql
@@ -0,0 +1,53 @@
+﻿----Write an SQL statement to create a stored procedure with name prcGetAllCustomers to display all records from Customer table.
+create or alter procedure prcGetAllCustomers
+as 
+Begin 
+Select * from Customers
+End
+
+exec prcGetAllCustomers;
+
+----Write an SQL statement to create a parameterized stored procedure with name prcGetCustomer which should take CustomerID as
+----parameter and displays the respective record based on that CustomerID.
+create procedure prcGetCustomer5(@CusID nvarchar(20))
+as 
+select * from Customers where CustomerID=@CusID
+
+exec prcGetCustomer5 'ALFKI'
+
+
+
+----Write an SQL statement to create a parameterized stored procedure with name prcAddCustomer which should take CustomerID, CustomerName, Email, Age, Phone as parameters and insert the record in Customer table.
+create procedure prcAddCustomer3 @CusId nvarchar(20), @CusName nvarchar(20), @Phone nvarchar(20), @Email nvarchar(20), @Age nvarchar(20)
+as 
+insert into Customers(CustomerId,ContactName,Phone,Age,Email) values(@CusId,@CusName,@Phone,@Age,@Email)
+
+exec prcAddCustomer3 '101','dhruv','789130','20','dhruv@gmail.com'
+
+
+----Write an SQL statement to alter the procedure prcGetCustomer to change its functionality. Instead of taking parameter CustomerID it should take CustomerName and display the respective record based on that CustomerName.
+create or alter procedure prcGetCustomer @CusName nvarchar(20)
+as 
+select * from Customers where CustomerName=@CusName
+
+----Write an SQL statement to create a parameterized stored procedure with prcAddOrUpdateCustomer which should take CustomerID, CustomerName, Email, Age, Phone as parameters. If the record with same CustomerID already exists it should update the data otherwise it should insert a new record.
+create or alter procedure prcAddorUpdateCustomer @CusId nvarchar(20),@CusName nvarchar(20), @Email nvarchar(20), @Age nvarchar(20), @Phone nvarchar(20)
+as
+--if already there update
+declare @Already nvarchar(20)
+set @Already=(select ContactName from Customers where CustomerID=@CusName)
+if @Already is Null
+	begin
+		update Customers set ContactName=@CusName,Phone=@Phone,Email=@Email,@Age=Age  where CustomerID
+	end
+--else insert
+else 
+	begin
+	insert into Customers(CustomerId,ContactName,Phone,Age,Email) values(@CusId,@CusName,@Phone,@Age,@Email)
+	end
+
+----Write an SQL statement to delete procedure prGetAllCustomers.
+DROP PROCEDURE [prcGetAllCustomers];
+
+
+SELECT * FROM INFORMATION_SCHEMA.COLUMNS
