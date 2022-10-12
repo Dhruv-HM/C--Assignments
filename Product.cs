44e13064f7fbf6bf8cf1522025ff924b3f5df3ff
Dhruv Kanthaliya <dhruv.kanthaliya@happiestminds.com>
2022-10-12 14:06:51 +0530

    pro management io module

---
+++ Product.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities
+{
+    /*
+     * this serializable class models product data that includes product id, product name, price and in-stock status
+     */
+    [Serializable]
+    public class Product
+    {
+        /*
+         * define properties for Product model attributes 
+         */
+        public int ProductId { get; set; }
+        public int Price { get; set; }
+        public string ProductName { get; set; }
+        public bool InStock { get; set; }
+
+        public Product()
+            {
+            
+            }
+        public Product(int ProductId,string ProductName, int Price, bool InStock=true)
+        {
+            this.ProductId = ProductId;
+            this.ProductName = ProductName;
+            this.Price = Price;
+            this.InStock = InStock;
+        }
+
+
+
+
+    /*
+     * Override ToString() method to return string equivalent of product object containing product details
+     */
+
+    public override string ToString()
+        {
+            return $"Product Name: {this.ProductName},Price: {this.Price}, Instock: {this.InStock}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Repositories;
+using System;
+
+namespace ProductModuleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * code here is not mandatory but will help 
+             * to understand flow better
+             */
+        }
+
+        
+    }
+}
